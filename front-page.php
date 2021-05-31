cf6961d96ebe43a59f1f40d84b9bbf98d4948fa6
elkin1704 <elkin170417@gmail.com>
2021-05-31 10:50:33 -0500

    Iniciando proyecto

---
+++ front-page.php
@@ -0,0 +1,47 @@
+<?php get_header(); ?>
+
+<main> 
+
+    <div class="max-w-md mx-auto ... bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500 ..."> 
+    
+        <div class="w-4/18">
+            <img class="rounded-full" src="<?php echo get_template_directory_uri( )?>/assets/img/foto mia.jpg" alt="Imagen de perfil Elkin">
+        </div>
+        <div class="w-6/40"><br>
+            <p>Hola me llamo Elkin Romero tengo 20 años, me gusta jugar futbol, escucho todo genero de musica, mi comidad favorita es el encebollado.</p>
+        </div>
+    </div><br>
+    <br>
+
+    <div class="flex items=center space-x-10 bg-gradient-to-r from-yellow-100 via-red-200 to-pink-200 ..."> 
+        <div class="w-1/12">
+            <img class="rounded-full" src="<?php echo get_template_directory_uri( )?>/assets/img/reparacion.jpg" alt="Imagen de reparacion">
+        </div>
+        <div class="w-6/12">
+            <p>Tengo conocimientos y un poco de experiencia en arreglo de celulares debido a que sigue un curso en el que aprendi muchas tecnicas de reparacion.</p>
+        </div>
+    </div><br>
+    <br>
+
+    <div class="flex items=center space-x-10 bg-gradient-to-r from-yellow-300 via-red-700 to-pink-300 ..."> 
+        <div class="w-1/12">
+            <img class="rounded-full" src="<?php echo get_template_directory_uri( )?>/assets/img/mantenimiento.jpg" alt="Imagen de reparacion">
+        </div>
+        <div class="w-6/12">
+            <p>Me gusta investigar todo sobre el mantenimiento de computadoras tanto en hadware como en software.</p>
+        </div>
+    </div><br>
+    <br>
+    <div class="flex items=center space-x-10 bg-gradient-to-r from-yellow-300 via-red-200 to-pink-200 ..."> 
+        <div class="w-1/12">
+            <img class="rounded-full" src="<?php echo get_template_directory_uri( )?>/assets/img/porsifalta.jpg" alt="Imagen de reparacion">
+        </div>
+        <div class="w-6/12">
+            <p>Me gusta el anime en especial Dragon Ball Z y soy fanatico de Goku</p>
+        </div>
+    </div>
+ </main>
+
+ 
+
+<?php get_footer(); ?>
